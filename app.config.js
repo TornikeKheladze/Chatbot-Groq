47de4def2f7943e0a9c6aac45cd44fdda35f5fa6
TornikeKheladze <xeladze.tornike@gmail.com>
2025-05-18 21:51:08 +0400

    feat:splash screen,icon

--- app.config.js
+++ app.config.js
@@ -4,8 +4,24 @@ export default {
   expo: {
     name: "Chatbot",
     slug: "Chatbot",
+    icon: "./assets/gpt.png",
+    splash: {
+      image: "./assets/gpt.png",
+      resizeMode: "contain",
+      backgroundColor: "#ffffff",
+    },
     extra: {
       groqApiKey: process.env.GROQ_API_KEY,
+    },
+    android: {
+      package: "com.anonymous.Chatbot",
+      adaptiveIcon: {
+        foregroundImage: "./assets/gpt.png",
+        backgroundColor: "#ffffff",
+      },
+    },
+    ios: {
+      bundleIdentifier: "com.anonymous.Chatbot",
     },
   },
 };
